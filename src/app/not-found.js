3bfc48b491db59a540196f7d9743eae4a6753d2d
Donya <thisis2nya@gmail.com>
2024-11-30 17:01:19 +0330

    feat: re-desgin 404 not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function NotFound() {
-  return <div>Oops!</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-6">
+      <p className="text-7xl font-bold">404</p>
+      <p className="text-5xl">Page Not Found!</p>
+    </div>
+  );
 }
 
 export default NotFound;
