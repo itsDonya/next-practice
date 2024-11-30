a3dc941dd8e6d6488aa3d8fe36c495f8d14bd19a
Donya <thisis2nya@gmail.com>
2024-11-30 03:49:09 +0330

    work with not-found route

---
+++ src/app/posts/[postId]/comments/[commentId]/not-found.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CommentNotFound() {
+  return <div>CommentNotFound!</div>;
+}
+
+export default CommentNotFound;
--- src/app/posts/[postId]/comments/[commentId]/page.js
+++ src/app/posts/[postId]/comments/[commentId]/page.js
@@ -1,6 +1,12 @@
 import React from "react";
 
+import { notFound } from "next/navigation";
+
 function PostSingleCommentPage({ params }) {
+  if (!Number(params.commentId)) {
+    notFound();
+  }
+
   return (
     <div>
       Comment {params.commentId} for post {params.postId}
