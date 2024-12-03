f180f2f280efb26b0b8b7e6739a44f46b71b27f0
Donya <thisis2nya@gmail.com>
2024-12-04 02:27:13 +0330

    feat: create single product page with dynamic data based on id param

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-function ProductSinglePage() {
-  return <div>Product Single Page</div>;
+function ProductSinglePage({ params }) {
+  return (
+    <div className="flex flex-col items-start justify-start gap-4">
+      <h1 className="text-3xl text0neutral-400 font-bold">
+        Product {params.id}
+      </h1>
+
+      <p className="w-2/5 text-neutral-600">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
+        tenetur ut sequi deleniti porro magni laboriosam enim perspiciatis. Unde
+        iure doloremque optio est sunt fugiat obcaecati eum rerum, ut quo atque
+        alias harum asperiores? Fugiat soluta aliquid corporis cupiditate
+        voluptatem!
+      </p>
+
+      <button className="px-4 py-2 text-sm text-white bg-slate-500 rounded-md hover:brightness-125 transition-all">
+        Buy Now
+      </button>
+    </div>
+  );
 }
 
 export default ProductSinglePage;
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -7,8 +7,8 @@ function ProductsPage() {
       {[...Array(20).keys()].map((item) => {
         return (
           <Link href={`/products/${item + 1}`}>
-            <div className="w-full h-44 flex flex-col items-center justify-center gap-2 border rounded-lg">
-              <p className="text-4xl font-bold">{item + 1}</p>
+            <div className="w-full h-44 flex flex-col items-center justify-center gap-2 border border-neutral-400 rounded-lg">
+              <p className="text-4xl text-neutral-400 font-bold">{item + 1}</p>
             </div>
           </Link>
         );
