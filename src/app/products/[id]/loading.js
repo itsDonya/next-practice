bf27e01b9d9ed684e7f35504d9298cc610f5a4ad
Donya <thisis2nya@gmail.com>
2024-12-05 15:21:43 +0330

    feat: create simple loading for productsingle product page single

---
+++ src/app/products/[id]/loading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SingleProductLoading() {
+  return <div>loading product data ...</div>;
+}
+
+export default SingleProductLoading;
--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -9,21 +9,22 @@ export function generateMetadata({ params }) {
   };
 }
 
-function ProductSinglePage({ params }) {
+async function ProductSinglePage({ params }) {
+  const product = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: params.id,
+        title: `Product ${params.id}`,
+        description:
+          "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid tenetur ut sequi deleniti porro magni laboriosam enim perspiciatis. Unde iure doloremque optio est sunt fugiat obcaecati eum rerum, ut quo atque alias harum asperiores? Fugiat soluta aliquid corporis cupiditate voluptatem!",
+      });
+    }, 2000);
+  });
+
   return (
     <div className="flex flex-col items-start justify-start gap-4">
-      <h1 className="text-3xl text0neutral-400 font-bold">
-        Product {params.id}
-      </h1>
-
-      <p className="w-2/5 text-neutral-600">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-        tenetur ut sequi deleniti porro magni laboriosam enim perspiciatis. Unde
-        iure doloremque optio est sunt fugiat obcaecati eum rerum, ut quo atque
-        alias harum asperiores? Fugiat soluta aliquid corporis cupiditate
-        voluptatem!
-      </p>
-
+      <h1 className="text-3xl text0neutral-400 font-bold">{product.title}</h1>
+      <p className="lg:w-2/5 text-neutral-600">{product.description}</p>
       <button className="px-4 py-2 text-sm text-white bg-slate-500 rounded-md hover:brightness-125 transition-all">
         Buy Now
       </button>
